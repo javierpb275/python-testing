512eac582d9fef714950b16fec602cdcc16f88bd
javierpb275 <javierpb275@gmail.com>
2021-11-21 14:32:01 +0100

    exercise testing

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-def do_stuff(num=0):
-    try:
-        if num:
-            return num + 5
-        else:
-            return 'please enter number'
-    except ValueError as err:
-        return err
+import random
+
+def run_guess(guess, answer):
+    if  0 < guess < 11:
+        if guess == answer:
+            print('you are a genius!')
+            return True
+    else:
+        print('hey bozo, I said 1~10')
+        return False
 
 if __name__ == '__main__':
-    do_stuff(10)
-
+    answer = random.randint(1, 10)
+    while True:
+        try:
+            guess = int(input('guess a number 1~10:  '))
+            if (run_guess(guess, answer)):
+                break
+        except ValueError:
+            print('please enter a number')
+            continue
--- test.py
+++ test.py
@@ -1,32 +1,22 @@
 import unittest
 import main
 
-class TestMain(unittest.TestCase):
-    def setUp(self):
-        print('about to test a function')
+class TestGame(unittest.TestCase):
+    def test_input(self):
+        result = main.run_guess(5, 5)
+        self.assertTrue(result)
 
-    def test_do_stuff(self):
-        test_param = 10
-        result = main.do_stuff(test_param)
-        self.assertEqual(result, 15)
+    def test_input_wrong_guess(self):
+        result = main.run_guess(0, 5)
+        self.assertFalse(result)
 
-    def test_do_stuff2(self):
-        test_param = 'safasf'
-        result = main.do_stuff(test_param)
-        self.assertIsInstance(result, ValueError)
+    def test_input_wrong_number(self):
+        result = main.run_guess(11, 5)
+        self.assertFalse(result)
 
-    def test_do_stuff3(self):
-        test_param = None
-        result = main.do_stuff(test_param)
-        self.assertEqual(result, 'please enter number')
-
-    def test_do_stuff4(self):
-        test_param = ''
-        result = main.do_stuff(test_param)
-        self.assertEqual(result, 'please enter number')
-
-    def tearDown(self):
-        print('cleaning up')
+    def test_input_wrong_type(self):
+        result = main.run_guess('11', 5)
+        self.assertFalse(result)
 
 if __name__ == '__main__':
     unittest.main()
